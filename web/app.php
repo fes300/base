b642336558efb6265057cc9020f1967348ec4b24
fes300 <federicosordillo@alice.it>
2016-05-15 16:00:29 +0200

    prova

--- web/app.php
+++ web/app.php
@@ -87,31 +87,31 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
 // $userRepo = new UserRepository($app['db']);
 // $usersData = $userRepo->getUsersForSilex();
 
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
-   'security.firewalls' =>  array(
-       'admin' => array(
-           'pattern' => '^/.*',
-           'anonymous' => true,
-           'http' => true,
-           'form' => array(
-               'login_path' => '/login',
-               'check_path' => '/admin/login_check',
-               'always_use_default_target_path' => true,
-               'default_target_path' => '/'
-           ),
-           'logout' => array(
-               'logout_path' => '/admin/logout',
-               'invalidate_session' => true
-           ),
-           'remember_me' => array(
-                'key' => '44pinc3d229ebqbvqa6bpvlhp3'
-            ),
-            'users' => array(
-                // raw password is foo, just put $usersData after db is set
-                'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
-            )
-   )
-)));
+// $app->register(new Silex\Provider\SecurityServiceProvider(), array(
+//    'security.firewalls' =>  array(
+//        'admin' => array(
+//            'pattern' => '^/.*',
+//            'anonymous' => true,
+//            'http' => true,
+//            'form' => array(
+//                'login_path' => '/login',
+//                'check_path' => '/admin/login_check',
+//                'always_use_default_target_path' => true,
+//                'default_target_path' => '/'
+//            ),
+//            'logout' => array(
+//                'logout_path' => '/admin/logout',
+//                'invalidate_session' => true
+//            ),
+//            'remember_me' => array(
+//                 'key' => '44pinc3d229ebqbvqa6bpvlhp3'
+//             ),
+//             'users' => array(
+//                 // raw password is foo, just put $usersData after db is set
+//                 'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+//             )
+//    )
+// )));
 
 
 $app->register(new Silex\Provider\RememberMeServiceProvider());
@@ -130,9 +130,14 @@ $app->get('/', function ()  use ($app) {
     //     $user = $userRepo->getByUsername($app['user']->getUsername());
     // } else {$user = '';}
 
-    $user = $app['user']->getUsername();
+    // $user = $app['user']->getUsername();
+    $user = 'nd';
     return $app->render('home.twig', ['user'=>$user, 'page'=>'home']);
 });
+
+$app->get('/prova', function () use ($app) {
+    return $app->render('home.twig', ['user'=>'$user', 'page'=>'home']);
+})
 
 // build Admin controller
 $admin = new Admin($app);
